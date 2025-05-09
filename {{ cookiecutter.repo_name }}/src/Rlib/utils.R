08196fb8e9a30ab520793c12d91bd021d9d66a4d
Alexander McFarland <alex.925@gmail.com>
2025-05-09 10:42:16 -0400

    correct subchunkify

--- {{ cookiecutter.repo_name }}/src/Rlib/utils.R
+++ {{ cookiecutter.repo_name }}/src/Rlib/utils.R
@@ -140,19 +140,22 @@ add_commas <- function(df) {
   return(df)
 }
 
-subchunkify <- function(g, fig_height=7, fig_width=5) {
+
+
+subchunkify <- function(g, fig_height = 7, fig_width = 5) {
   g_deparsed <- paste0(deparse(
-    function() {g}
+    function() { g }
   ), collapse = '')
   
-  sub_chunk <- paste0("
-  `","``{r sub_chunk_", floor(runif(1) * 10000), ", fig.height=",
-  fig_height, ", fig.width=", fig_width, ", echo=FALSE, warning=FALSE, message=FALSE}",
-  "\n(", 
-  g_deparsed
-  , ")()",
-  "\n`","``
-  ")
+  sub_chunk <- paste0(
+    "```{r sub_chunk_", floor(runif(1) * 10000),
+    ", fig.height=", fig_height,
+    ", fig.width=", fig_width,
+    ", echo=FALSE, warning=FALSE, message=FALSE}\n",
+    "(", g_deparsed, ")()\n",
+    "```"
+  )
   
   cat(knitr::knit(text = knitr::knit_expand(text = sub_chunk), quiet = TRUE))
 }
+
